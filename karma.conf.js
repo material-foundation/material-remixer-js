e352aff3f4759f8696d69862fb50cecb720cddcc
Chris Cox <cjcox@google.com>
2016-11-29 14:40:26 -0800

    Testing travis with phantomjs only

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,7 @@ module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['mocha'],
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
     reporters: ['progress'],
     client: {
       mocha: {
