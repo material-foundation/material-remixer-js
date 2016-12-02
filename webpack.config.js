dc97c3ffab94f76cc108fcbd7869ea137320563d
Chris Cox <cjcox@google.com>
2016-12-02 09:17:35 -0800

    Updates file formatting.

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,50 @@
+/** @license
+ *  Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the 'License'); you may not
+ *  use this file except in compliance with the License. You may obtain a copy
+ *  of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ */
+
+'use strict';
+
 module.exports = {
   entry: {
     remixer: './src/core/Remixer.ts',
     overlay: './src/ui/render.tsx'
   },
   output: {
-    path: __dirname + "/dist",
-    filename: "[name].js",
+    path: __dirname + '/dist',
+    filename: '[name].js',
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   resolve: {
-    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
   },
   module: {
     loaders: [{
       test: /\.tsx?$/,
-      loader: "ts-loader"
+      loader: 'ts-loader'
     }, {
       test: /\.less$/,
-      loader: "style-loader!css-loader!less-loader"
+      loader: 'style-loader!css-loader!less-loader'
     }, {
       test: /\.html$/,
-      loader: "html-loader"
+      loader: 'html-loader'
     }],
   },
   externals: {
-    "react": "React",
-    "react-dom": "ReactDOM",
+    'react': 'React',
+    'react-dom': 'ReactDOM',
   }
 };
